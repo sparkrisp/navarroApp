52557cfe813ff73057cb231bb2a9a5a49b0e8668
Ariel Ivan Ruiz Mateos <arielrm@gmail.com>
2020-07-07 03:53:42 +0200

    Add files via upload

---
+++ app/scripts/applangs.js
@@ -0,0 +1,32 @@
+﻿/*!
+ This file is part of DecSoft App Builder.
+ Visit our website for license information.
+ Copyright ©2020 DecSoft. All rights reserved.
+ Visit our website at: https://www.davidesperalta.com
+ */
+
+window.appLangs = [];
+
+window.appLangs["es"] = {
+
+  language: {
+    name: "Spanish"
+  },
+
+  resources: {
+    vermas: function(id){
+if(id=="mas"){
+document.getElementById("desplegar").style.display="block";
+document.getElementById("mas").style.display="none";
+}
+else{
+document.getElementById("desplegar").style.display="none";
+document.getElementById("mas").style.display="inline";
+}
+}
+  },
+  sidebar: {
+    items: []
+  }
+};
+// End of window.appLangs["es"] = {
